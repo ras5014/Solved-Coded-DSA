5ad463b5fb6c9fe593f8c877a425fd4381e1e139
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2023-09-30 20:28:52 +0530

    Time: 0 ms (100%), Space: 12.7 MB (10%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,6 +3,8 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int break_point = -1;
+
+
         // Finding Breaking Point
         for(int i = n-2; i >= 0; i--) {
             if(nums[i] < nums[i+1]) {
@@ -10,11 +12,15 @@ public:
                 break;
             }
         }
+
+
         // If No break point found (ex: 54321 -> return 12345)
         if(break_point == -1) {
             reverse(nums.begin(), nums.end());
             return;
         }
+
+
         // if break point found -> find smallest greater than breaking point from right side and swap with a[i]
         for(int i = n-1; i > break_point; i--) {
             if(nums[i] > nums[break_point]) {
@@ -22,6 +28,8 @@ public:
                 break;
             }
         }
+
+        
         // reverse from i+1 to n-1
         reverse(nums.begin()+break_point+1, nums.end());
     }
