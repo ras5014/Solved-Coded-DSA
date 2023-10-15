251f4bd8a0180a4b3b014b053bb862dc7e6b3f2b
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2023-10-16 00:15:15 +0530

    Time: 10 ms (70.86%), Space: 11.5 MB (15.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
         int n = nums.size();
-
-        // Storing value and it's index
         map<int, int> mpp;
 
-        // Looping through the array and checking if target-nums[i] present in map
+        // Checking if target-element is present or not
         for(int i = 0; i < n; i++) {
             if(mpp.find(target-nums[i]) != mpp.end()) {
-                ans.push_back(i);
-                ans.push_back(mpp[target-nums[i]]);
+                return {mpp[target-nums[i]], i};
             } else {
-                mpp[nums[i]] = i; // Storing the index
+                mpp[nums[i]] = i;
             }
         }
-        sort(ans.begin(), ans.end());
-        return ans;
+        return {-1, -1};
     }
 };
